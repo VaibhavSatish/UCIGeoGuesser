ecc2dd5e4ae34e6d97b4c1edeb2cc5220a2fb856
Anish Venkatesalu <anivcs50@gmail.com>
2026-05-13 12:01:44 -0700

    Editied scripts to always start from script directory

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 echo "Building Docker image..."
+cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" || exit 1
+cd ../backend 
 docker build -t ucigeoguesser --target builder .
 echo "Building Docker container..."
+docker stop backend
+docker rm backend
 docker run -p 18080:18080 -d --name backend -v .:/backend ucigeoguesser sleep infinity
 echo "Accessing Docker container..."
 docker exec -it backend bash
+
--- scripts/clean-docker.sh
+++ scripts/clean-docker.sh
@@ -1,5 +1,7 @@
-#!bin/bash
+#!/bin/bash
 echo "Cleaning up existing Docker container..."
+cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" || exit 1
+cd ../backend 
 docker stop backend
 docker rm backend
 echo "Deleting existing Docker image..."
